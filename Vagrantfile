1cf50b2d499d390b47cf2b6817b4c8051a4a3e0d
Alexander Wolff <wolffaxn@gmail.com>
2016-02-21 19:40:17 +0100

    install and configure ansible

--- Vagrantfile
+++ Vagrantfile
@@ -32,6 +32,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
       vb.customize ["modifyvm", :id, "--nictype1", "virtio"]
       vb.customize ["modifyvm", :id, "--nictype2", "virtio"]
     end
+    mgmt_config.vm.provision :shell, path: "bootstrap-ansible.sh"
   end
 
   # create load balancer node
---
+++ bootstrap-ansible.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# install ansible (http://docs.ansible.com/intro_installation.html)
+apt-get -y install software-properties-common
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get -y install ansible
+
+# configure hosts file for our internal network
+cat << EOF > /etc/hosts
+
+# vagrant environment nodes
+10.0.15.10  mgmt
+10.0.15.11  lbl
+10.0.15.21  web1
+10.0.15.22  web2
+10.0.15.23  web3
+10.0.15.24  web4
+EOF
